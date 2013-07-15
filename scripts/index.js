fbe9aad28d9201ada4eac8b125b68726a92cb7ba
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2013-07-15 15:18:24 -0500

    New START!
        - Removed not needed files and libs
        - Added new file structure
        - Added new imple and clean layout
        - Added new Logo
        - Added nasic Home page with audio sample (not working yet)

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+/*
+            Obra de Guadalajara : www.obradeguadalajara.mx
+                          Some Rights Reserved
+                        Creative Commons License
+       Atribución-No comercial-Licenciamiento Recíproco 2.5 México
+                                                                      */
+
+/*global $, console*/
+$(function(){
+    console.log('OdeG - APP Started');
+
+    $('ul#lista-predicaciones li a').on('click', function(e){
+
+    });
+});
