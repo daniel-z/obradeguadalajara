93b844d542e131671c4c43d0fe63f49e5dba230a
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2013-07-22 21:14:25 -0500

    Adding social icons to page header.

---
+++ scripts/home.js
@@ -0,0 +1,37 @@
+/*
+            Obra de Guadalajara : www.obradeguadalajara.mx
+                          Some Rights Reserved
+                        Creative Commons License
+       Atribución-No comercial-Licenciamiento Recíproco 2.5 México
+                                                                      */
+
+/*global $, AudioApp, Logger, Notify, Modernizr*/
+$(function(){
+
+    var myAudioApp = new AudioApp({
+        audioBasePath: 'audio/',
+        audioControl: 'audio#audio-player',
+        audioList: 'ul#preaching-list',
+        audioMonitor: 'div.audio-preaching span.song'
+    }),
+    myLogger = new Logger({
+        'module':'OdeG Main App'
+    }),
+    myNotify = new Notify({wrapper: 'div#page-notifications'});
+
+    // ToDo: remove next line for production environment
+    myLogger.turnOn();
+    myLogger.log('APP Start');
+
+    myNotify.warning('Esta p&aacute;gina esta en desarrollo, gracias por tu paciencia!', 4000);
+
+    // load audio data
+    if (Modernizr.audio.mp3){
+        $.getJSON('audio/audiodata.json', function(data) {
+            myAudioApp.init(data);
+        });
+    } else {
+        $('div.audio-preaching').addClass('hidden');
+        $('p.audio-notification').removeClass('hidden');
+    }
+});
--- scripts/index.js
+++ scripts/index.js
@@ -8,30 +8,10 @@
 /*global $, AudioApp, Logger, Notify, Modernizr*/
 $(function(){
 
-    var myAudioApp = new AudioApp({
-        audioBasePath: 'audio/',
-        audioControl: 'audio#audio-player',
-        audioList: 'ul#preaching-list',
-        audioMonitor: 'div.audio-preaching span.song'
-    }),
-    myLogger = new Logger({
-        'module':'OdeG Main App'
-    }),
+    var myLogger = new Logger({ 'module':'OdeG Main App' }),
     myNotify = new Notify({wrapper: 'div#page-notifications'});
 
     // ToDo: remove next line for production environment
     myLogger.turnOn();
     myLogger.log('APP Start');
-
-    myNotify.warning('Esta p&aacute;gina esta en desarrollo, gracias por tu paciencia!', 4000);
-
-    // load audio data
-    if (Modernizr.audio.mp3){
-        $.getJSON('audio/audiodata.json', function(data) {
-            myAudioApp.init(data);
-        });
-    } else {
-        $('div.audio-preaching').addClass('hidden');
-        $('p.audio-notification').removeClass('hidden');
-    }
 });
