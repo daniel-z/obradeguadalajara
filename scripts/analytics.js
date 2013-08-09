93624717f5578c22b4e94aed0210749cb35e2e5e
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2013-08-09 10:55:19 -0500

    Updated analytics code.

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-32852224-2', 'obradeguadalajara.com');
+ga('create', 'UA-43005881-1', 'obradeguadalajara.com');
 ga('send', 'pageview');
