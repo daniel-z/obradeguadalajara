cc499ada462922f4851d6e82a5aa6a833e224c37
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2013-11-10 18:18:08 -0600

    closes #8 - Added audio page with 5 initial audio samples

--- scripts/audio.js
+++ scripts/audio.js
@@ -8,13 +8,39 @@
 /*global $, AudioApp, Logger, Notify, Modernizr*/
 $(function(){
 
-    var myAudioApp = new AudioApp({
-        audioBasePath: 'audio/',
-        audioControl: 'audio#audio-player',
-        audioList: 'ul#preaching-list',
-        audioMonitor: 'div.audio-preaching span.song'
-    }),
-    myLogger = new Logger({
+    var loadAudioPlayer = function(audiodata) {
+        var cssSelector = { jPlayer: "#jquery_jplayer_1", cssSelectorAncestor: "#jp_container_1" },
+        playlist = audiodata,
+        options = {
+            swfPath: "/scripts/jplayer",
+            supplied: "mp3",
+            autoPlay: false,
+            loopOnPrevious: false,
+            shuffleOnLoop: false,
+            enableRemoveControls: false,
+            displayTime: 'slow',
+            addTime: 'fast',
+            removeTime: 'fast',
+            shuffleTime: 'slow'
+        },
+        myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
+
+        $("#jquery_jplayer_1").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                  mp3: "/audio/2013-05-05-UnPerfumeMuyEspecial.mp3"
+                });
+            },
+            swfPath: "/scripts/lib/jplayer",
+            supplied: "mp3"
+        });
+    };
+
+    $.getJSON( "audio/audiodata.json", function( data ) {
+        loadAudioPlayer(data);
+    });
+
+    var myLogger = new Logger({
         'module':'OdeG Main App'
     }),
     myNotify = new Notify({wrapper: 'div#page-notifications'});
@@ -22,14 +48,4 @@ $(function(){
     // ToDo: remove next line for production environment
     myLogger.turnOn();
     myLogger.log('APP Start');
-
-    // load audio data
-    if (Modernizr.audio.mp3){
-        $.getJSON('audio/audiodata.json', function(data) {
-            myAudioApp.init(data);
-        });
-    } else {
-        $('div.audio-preaching').addClass('hidden');
-        $('p.audio-notification').removeClass('hidden');
-    }
 });
